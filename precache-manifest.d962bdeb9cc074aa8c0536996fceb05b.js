362222dbdec45fa9572ffe16cd7fa47d425ddd48
Thomas Laukkanen <tumppu91@hotmail.com>
2021-02-04 22:57:27 +0100

    final site

---
+++ precache-manifest.d962bdeb9cc074aa8c0536996fceb05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fa6aedae2e96a1974c",
+    "url": "/css/app.ed8c3b49.css"
+  },
+  {
+    "revision": "32044aec957a359e5ec8223a39ac5c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eac4d4c87df9e7f8c62",
+    "url": "/js/about.7582c44f.js"
+  },
+  {
+    "revision": "04fa6aedae2e96a1974c",
+    "url": "/js/app.1e3d9e4c.js"
+  },
+  {
+    "revision": "8f900f6b5b3f59f30150",
+    "url": "/js/chunk-vendors.75fec746.js"
+  },
+  {
+    "revision": "111e78a56aff7c0b21489733e5aeece2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
