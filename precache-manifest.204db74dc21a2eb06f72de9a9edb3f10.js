293fc6a80d43c70524478988d2ee30186e462147
Thomas Laukkanen <tumppu91@hotmail.com>
2021-05-18 23:01:48 +0200

    new site

---
+++ precache-manifest.204db74dc21a2eb06f72de9a9edb3f10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6dfc5277da6d8e17475",
+    "url": "/css/app.16fe7832.css"
+  },
+  {
+    "revision": "d4058a07f751527253a7a7f8414f325f",
+    "url": "/img/goteborg.d4058a07.png"
+  },
+  {
+    "revision": "07b90561afb332bf050d21b4ae2cdca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0ccc1b75eed5775db30",
+    "url": "/js/about.4a3b9e33.js"
+  },
+  {
+    "revision": "f6dfc5277da6d8e17475",
+    "url": "/js/app.2aaf35f1.js"
+  },
+  {
+    "revision": "489792c6c4413d61bd63",
+    "url": "/js/chunk-vendors.42e62ce5.js"
+  },
+  {
+    "revision": "111e78a56aff7c0b21489733e5aeece2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
